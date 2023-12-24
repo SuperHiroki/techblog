1b12d722109d079a9c86b73c9de74dbc84e8e892
Ubuntu <jh147896325jh@gmail.com>
2023-12-24 02:34:53 +0000

    first commit

---
+++ mymemo/1.py
@@ -0,0 +1,23 @@
+import feedparser
+
+# RSSフィードのURL（正しいURLに置き換えてください）
+feed_url = "https://techblog.zozo.com/rss"
+
+# フィードを解析
+feed = feedparser.parse(feed_url)
+
+# フィードの解析エラーをチェック
+if feed.bozo:
+    print(f"Error parsing feed: {feed.bozo_exception}")
+else:
+    # エントリが存在するかチェック
+    if len(feed.entries) == 0:
+        print("No entries found in feed.")
+    else:
+        # 各記事のタイトルとリンクを表示
+        for entry in feed.entries:
+            print(f"Title: {entry.title}")
+            print(f"Link: {entry.link}")
+            published = entry.get("published", "Not available")
+            print(f"Published: {published}")
+            print("----------------------")
