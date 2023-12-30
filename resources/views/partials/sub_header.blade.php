a122f9e95c68e866e084b3a8298b40c4c9c3794a
Ubuntu <jh147896325jh@gmail.com>
2023-12-30 03:36:01 +0000

    To begin with, a dropdown closes when you click somewhere else. So it's no surprise that the dropdown closes when you press COLLAPSE.

--- resources/views/partials/sub_header.blade.php
+++ resources/views/partials/sub_header.blade.php
@@ -1,7 +1,7 @@
-<!-- resources/views/my-page/partials/header.blade.php -->
+<!-- resources/views/layouts/partials/sub_header.blade.php -->
 
 <!-- ヘッダー -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light bg-white shadow-lg rounded m-2 p-2">
+<nav id="aaa" class="navbar navbar-expand-lg navbar-light bg-light bg-white shadow-lg rounded m-2 p-2">
     <!-- ハンバーガーメニュー -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,9 +1,65 @@
-<!DOCTYPE html>
-<html>
+<!-- resources/views/test.blade.php -->
+
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Test Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
+
+
 <body>
-    <h1>Hello Test!!</h1>
+
+
+<!-- コンテンツ -->
+<div class="container mt-5">
+    <!-- 最初のコラプス -->
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        クリックしてコラプスを表示
+    </button>
+    <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+            通常のコンテンツ
+            <!-- ドロップダウン -->
+            <div class="dropdown mt-3">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    ドロップダウン
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="#">アクション1</a></li>
+                    <li><a class="dropdown-item" href="#">アクション2</a></li>
+                    <li><a class="dropdown-item" href="#">アクション3</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- 別のコラプス -->
+    <button class="btn btn-primary mt-4" type="button" data-bs-toggle="collapse" data-bs-target="#anotherCollapse" aria-expanded="false" aria-controls="anotherCollapse">
+        別のコラプスを表示
+    </button>
+    <div class="collapse" id="anotherCollapse">
+        <div class="card card-body">
+            別のコラプスのコンテンツ
+        </div>
+    </div>
+</div>
+
+
+
 </body>
 </html>
