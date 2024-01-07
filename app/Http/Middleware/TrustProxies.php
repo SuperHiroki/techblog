5bbdacaff539684c6c4fd5d20fa24553afe5e557
Ubuntu <jh147896325jh@gmail.com>
2024-01-07 22:06:34 +0000

    Force HTTPS, because there is mix of http and https unless I do not force https.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,8 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    //protected $proxies;
+    protected $proxies = '*'; // 信頼するプロキシを全てに設定
 
     /**
      * The headers that should be used to detect proxies.
